8c44bfeaae4dc3c38ce23e8f5eb0b7ec7070feba
RafaelReyes08 <00176326@uca.edu.sv>
2026-05-18 15:36:37 -0600

    Ejercicio 10 completo

--- For10.cpp
+++ For10.cpp
@@ -2,19 +2,23 @@
 using namespace std;
 int main ()
 {
-    int serie=0, sig;
+    int serie=0, sig, primero, segundo;
 
     cout << "Ingrese que tantos terminos de la serie desea ver" << endl;
     cin >> serie;
 
-    for (int i=1; i<=serie; i++)
+    primero=0;
+    segundo=1;
+        
+    cout << primero << endl;
+    cout << segundo << endl;
+
+    for (int i=1; i<=(serie-2); i++)
     {
-        a=1;
-        b=2;
         sig=primero+segundo;
-        segundo=primero;
-        sig=segundo;
-        cout <<
+        primero=segundo;
+        segundo=sig;
+        cout << sig << endl;
     }
 
     return 0;
